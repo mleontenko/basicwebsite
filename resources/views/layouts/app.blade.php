ffd5add8e1cd8a6dedc65260c892ba2a54f8f4e5
Marin Leontenko <marin.leontenko@gmail.com>
2018-01-18 15:16:52 +0100

    layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Acme</title>
+    </head>
+    <body>
+        @yield('content')
+
+        @section('sidebar')
+            <div class="sidebar">
+                <h3>Sidebar</h3>
+                This is the sidebar
+                @show
+            </div>
+    </body>
+</html>
